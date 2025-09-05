8090de736adf7a7a6a5eef3cee389857371c93db
Tyler Rafferty <tjraff5@Gmail.com>
2025-09-04 22:21:23 -0400

    update

--- Backend/middleware/cors.go
+++ Backend/middleware/cors.go
@@ -17,7 +17,7 @@ func WithCORS(next http.HandlerFunc) http.HandlerFunc {
 		if allowedOrigins[origin] {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
