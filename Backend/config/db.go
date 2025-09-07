73c64b12b9567e6678c319e07314f865ef2e1201
Tyler Rafferty <tjraff5@Gmail.com>
2025-09-07 06:04:02 -0400

    more verbose connection to db

--- Backend/config/db.go
+++ Backend/config/db.go
@@ -12,25 +12,29 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
-		"aws-1-us-east-1.pooler.supabase.com",
-		"postgres.rjkunrqftevwfssronze",
-		"#k4V6neSN8*5v4q",
-		"postgres",
-		"6543",
-	)
+	log.Println("Attempting to connect to the database with hardcoded credentials...")
 
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-		// This is the key change to prevent the prepared statement conflict
-		// It tells GORM to not use a prepared statement cache
-		// which is a common source of conflict with connection poolers like Supabase's
-		PrepareStmt: false, 
-	})
-	if err != nil {
-		log.Fatal("Failed to connect to DB:", err)
-	}
+    // This is the hardcoded DSN string
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+        "aws-1-us-east-1.pooler.supabase.com",
+        "postgres.rjkunrqftevwfssronze",
+        "#k4V6neSN8*5v4q",
+        "postgres",
+        "6543",
+    )
+    
+    // Log the configuration for debugging
+    log.Printf("Using hardcoded DSN. PrepareStmt will be set to false.")
+
+    var err error
+    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+        // This is the crucial fix for the "prepared statement" error
+        PrepareStmt: false, 
+    })
+    if err != nil {
+        log.Fatal("Failed to connect to DB:", err)
+    }
 
 	err = DB.AutoMigrate(
 		&models.User{},
