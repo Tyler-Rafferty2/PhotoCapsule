378a31ee5abf3ef83a035f30d04fb99377912d38
Tyler Rafferty <tjraff5@gmail.com>
2025-09-13 22:14:57 -0400

    update analytics and title adn desc

--- Frontend/src/app/layout.js
+++ Frontend/src/app/layout.js
@@ -3,9 +3,10 @@
 import "./globals.css";
 import { AuthProvider } from "@/context/authContext"
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
-  title: "PhotoVault",
-  description: "Image sharing app",
+  title: "MyPhotoCapsule",
+  description: "Photo Capsule is a simple and secure way to store and share your memories online. Create digital time capsules, organize your photos, and revisit them anytime."
 };
 
 export default function RootLayout({ children }) {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <AuthProvider>
           <Suspense fallback={<div>Loading...</div>}>
             {children}
+            <Analytics />
           </Suspense>
         </AuthProvider>
       </body>
