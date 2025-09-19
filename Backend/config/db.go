06fc8659da21e5d81166863ab5c865a1af177bcb
Tyler Rafferty <tjraff5@gmail.com>
2025-09-19 14:35:26 -0400

    fixed test bucket

--- Backend/config/db.go
+++ Backend/config/db.go
@@ -25,15 +25,14 @@ func ConnectToDB() {
     
     // Log the configuration for debugging
     log.Printf("PreferSimpleProtocol will be set to true.")
-	if(os.Getenv("APP_ENV") != "test"){
-		var err error
-		DB, err = gorm.Open(postgres.New(postgres.Config{
-			DSN:                  dsn,
-			PreferSimpleProtocol:  true, // disables implicit prepared statement usage
-		}), &gorm.Config{})
-		if err != nil {
-			log.Fatal("Failed to connect to DB:", err)
-		}
-	}
+
+    var err error
+	DB, err = gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol:  true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to DB:", err)
+    }
 	fmt.Println("✅ Connected to PostgreSQL database with GORM!")
 }
