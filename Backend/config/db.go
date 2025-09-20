9b60f1ed3853dde5ad853f25bdb9ead432a3f5b5
Tyler Rafferty <tjraff5@gmail.com>
2025-09-20 12:10:47 -0400

    chang postgres setup

--- Backend/config/db.go
+++ Backend/config/db.go
@@ -13,26 +13,48 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	log.Println("Attempting to connect to the database with hardcoded credentials...")
+    if(os.Getenv("APP_ENV") == "test")
+	{
+			dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			GetEnv("DB_HOST", "localhost"),
+			GetEnv("DB_USER", "postgres"),
+			GetEnv("DB_PASSWORD", "password"),
+			GetEnv("DB_NAME", "timecapsule"),
+			GetEnv("DB_PORT", "5432"),
+		)
 
-    // This is the hardcoded DSN string
-    user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	dbname := os.Getenv("DB_NAME")
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatal("Failed to connect to DB:", err)
+		}
 
-	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", user, password, host, port, dbname)
-    
-    // Log the configuration for debugging
-    log.Printf("PreferSimpleProtocol will be set to true.")
+		if err := DB.AutoMigrate(&models.User{}, &models.Vault{}, &models.Upload{}, &models.CoverImage{}, &models.RefreshToken{}); err != nil {
+			log.Fatal("Auto-migration failed:", err)
+		}
+		
+	}else{
+		// This is the hardcoded DSN string
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		host := os.Getenv("DB_HOST")
+		port := os.Getenv("DB_PORT")
+		dbname := os.Getenv("DB_NAME")
 
-    var err error
-	DB, err = gorm.Open(postgres.New(postgres.Config{
-		DSN:                  dsn,
-		PreferSimpleProtocol:  true, // disables implicit prepared statement usage
-	}), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to DB:", err)
-    }
+		dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", user, password, host, port, dbname)
+		
+		// Log the configuration for debugging
+		log.Printf("PreferSimpleProtocol will be set to true.")
+
+		var err error
+		DB, err = gorm.Open(postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol:  true, // disables implicit prepared statement usage
+		}), &gorm.Config{})
+		if err != nil {
+			log.Fatal("Failed to connect to DB:", err)
+		}
+	}
+
 	fmt.Println("✅ Connected to PostgreSQL database with GORM!")
 }
