1027f2b3c18874940bc616ead6bd1715b8705348
Tyler Rafferty <tjraff5@gmail.com>
2025-09-21 12:58:14 -0400

    updated images for google seo

--- Frontend/src/app/layout.js
+++ Frontend/src/app/layout.js
@@ -1,17 +1,25 @@
 // src/app/layout.js
-
 import "./globals.css";
-import { AuthProvider } from "@/context/authContext"
+import { AuthProvider } from "@/context/authContext";
 import { Suspense } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+
 export const metadata = {
   title: "MyPhotoCapsule",
-  description: "Photo Capsule is a simple and secure way to store and share your memories online. Create digital time capsules, organize your photos, and revisit them anytime."
+  description:
+    "Photo Capsule is a simple and secure way to store and share your memories online. Create digital time capsules, organize your photos, and revisit them anytime.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* Optional: different sizes for Google */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
       <body>
         <AuthProvider>
           <Suspense fallback={<div>Loading...</div>}>
