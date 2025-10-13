4a0b8904ff913bf9e8d5e24cc2076692a0b61397
Tyler Rafferty <tjraff5@gmail.com>
2025-10-13 12:18:18 -0400

    added vars

---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  description = "AWS Access Key for authentication"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key for authentication"
+  type        = string
+}
